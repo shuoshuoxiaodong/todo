7ac50f7ffd23ba56039dff69ee2d91d8f4d5281a
shuoshuoxiaodong <2544656002@qq.com>
2019-06-18 10:35:45 +0800

    防止超链接名字重复

--- tu/urls.py
+++ tu/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name='tu'
+
+
 urlpatterns = [
-    path('home/',views.home),
-    path('edit/',views.edit),
-    path('about/',views.about)
+    path('home/',views.home,name='主页'),
+    path('edit/',views.edit,name='编辑'),
+    path('about/',views.about,name='关于')
 ]
--- tu/views.py
+++ tu/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'tu/home.html')
 
 
 def edit(request):
-    return render(request,'edit.html')
+    return render(request, 'tu/edit.html')
 
 def about(request):
-    return render(request,'about.html')
+    return render(request, 'tu/about.html')
