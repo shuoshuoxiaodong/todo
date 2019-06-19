510ebf516c3f307958db33bda10d10b4fb6b7a4e
shuoshuoxiaodong <2544656002@qq.com>
2019-06-19 11:47:45 +0800

    连接数据库

--- tu/models.py
+++ tu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class TODO(models.Model):
+    thing = models.CharField(max_length=20, verbose_name='待办事项')
+    done = models.BooleanField(default=False ,verbose_name='已完成')
+    def _str_(self):
+        return self.thing
+    class Meta():
+        db_table = 'tu'
