0d4d761d8045d8ea36176cdaf24210bcad3cf8bc
shuoshuoxiaodong <2544656002@qq.com>
2019-06-19 09:05:13 +0800

    删除表单按钮、修改信息

--- tu/urls.py
+++ tu/urls.py
@@ -7,6 +7,7 @@ app_name='tu'
 
 urlpatterns = [
     path('home/',views.home,name='主页'),
-    path('edit/',views.edit,name='编辑'),
-    path('about/',views.about,name='关于')
+    path('edit/<int:forloop_counter>',views.edit,name='编辑'),
+    path('about/',views.about,name='关于'),
+    path('del/<forloop_counter>',views.delete,name='删除')
 ]
--- tu/views.py
+++ tu/views.py
@@ -1,12 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+lst = [
+    {'待办事项': '吃饭', '已完成': True},
+    {'待办事项': '逛街', '已完成': False},
+
+]
+
+
 def home(request):
-    return render(request, 'tu/home.html')
+    global lst
+    if request.method == 'POST':
+        content = request.POST.get('待办事项')
+        if not content or content.strip() == '':
+            return render(request, 'tu/home.html', {'清单': lst, '警告': '请输入内容'})
+        else:
+            lst.append({'待办事项': content, '已完成': False})
+            return render(request, 'tu/home.html', {'清单': lst})
+    else:
+        return render(request, 'tu/home.html', {'清单': lst})
 
 
-def edit(request):
-    return render(request, 'tu/edit.html')
+def edit(request, forloop_counter):
+    if request.method == 'POST':
+        content = request.POST['已修改事项']
+        if not content or content.strip() == '':
+            return render(request, 'tu/edit.html', {'警告': '请输入内容'})
+        else:
+            lst[forloop_counter - 1]['待办事项'] = content
+            return redirect('tu:主页')
+    else:
+        content = lst[forloop_counter - 1]['待办事项']
+        return render(request, 'tu/edit.html', {'待修改事项': content})
+
 
 def about(request):
     return render(request, 'tu/about.html')
+
+
+def delete(request, forloop_counter):
+    if request.method == 'POST':
+        forloop_counter = int(forloop_counter) - 1
+        lst.pop(forloop_counter)
+    else:
+        return redirect('tu:主页')
